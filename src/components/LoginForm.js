7dad901744ff267dc0063b38fdf3293e7f5de175
Joshua Peavler <joshuaapeavler@gmail.com>
2020-04-14 10:17:20 -0600

    add functionality to login form

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,14 +1,42 @@
 import React from 'react';
 
-function LoginForm() {
-    return(
-        <form>
-            <h3>Login here</h3>
-            <input type="text" placeholder="Email" required></input>
-            <input type="text" placeholder="Password" required></input>
-            <input type="submit" value="Login"></input>
-        </form>
-    );
+class LoginForm extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {value: ''};
+
+        this.handleEmail = this.handleEmail.bind(this);
+        this.handlePassword = this.handlePassword.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    handleEmail(event) {
+        this.setState({email: event.target.value});
+    }
+    handlePassword(event) {
+        this.setState({password: event.target.value});
+    }
+    handleSubmit(event) {
+        console.log({
+            email: this.state.email,
+            password: this.state.password
+        })
+        event.preventDefault();
+    }
+
+    render(){
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <h3>Login here</h3>
+                <input type="email" placeholder="Email: email@website.com" required
+                        pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+                        name="Email" onChange={this.handleEmail}/>
+                <input type="password" placeholder="Password" required
+                        pattern=".{8,}" name="Password" onChange={this.handlePassword}/>
+                <input type="submit" value="Login"/>
+            </form>
+        );
+    }
 }
 
 export default LoginForm;
