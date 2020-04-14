c99e889542046eba043668634cf9e716f322c549
Joshua Peavler <joshuaapeavler@gmail.com>
2020-04-14 10:18:01 -0600

    add functionality to ForgotPasswordForm

--- src/components/ForgotPasswordForm.js
+++ src/components/ForgotPasswordForm.js
@@ -1,13 +1,35 @@
 import React from 'react';
 
-function ForgotPasswordForm() {
-    return(
-        <form>
-            <h3>Fogot Password? Request New Password Here</h3>
-            <input type="text" placeholder="Email" required></input>
-            <input type="submit" value="Change Password"></input>
-        </form>
-    );
+class ForgotPasswordForm extends React.Component {
+    constructor(props){
+        super(props);
+        this.state = {value: ''};
+
+        this.handleEmail = this.handleEmail.bind(this);
+        this.handleSubmit = this.handleSubmit.bind(this);
+    }
+    
+    handleEmail(event){
+        this.setState({email: this.state.value});
+    }
+    handleSubmit(event){
+        console.log({
+            email: this.state.email,
+        })
+        event.preventDefault();
+    }
+
+    render(){
+        return(
+            <form onSubmit={this.handleSubmit}>
+                <h3>Fogot Password? Request New Password Here</h3>
+                <input type="email" placeholder="Email: email@website.com" required
+                        pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,}$"
+                        name="Email" onChange={this.handleEmail}/>
+                <input type="submit" value="Change Password"/>
+            </form>
+        );
+    }
 }
 
 export default ForgotPasswordForm;
