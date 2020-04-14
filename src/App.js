0aab5f3421aa52d2d9ac65abe935c39e7f2a356f
Joshua Peavler <joshuaapeavler@gmail.com>
2020-04-14 10:55:52 -0600

    add routing to App

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
 import CreateAccountForm from './components/CreateAccountForm';
 import LoginForm from './components/LoginForm';
@@ -8,12 +14,41 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <CreateAccountForm/>
-        <LoginForm/>
-        <ForgotPasswordForm/>
+      <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Login</Link>
+            </li>
+            <li>
+              <Link to="/create">Create Account</Link>
+            </li>
+            <li>
+              <Link to="/forgotpassword">Forgot Password?</Link>
+            </li>
+          </ul>
+        </nav>
+
+        {/* A <Switch> looks through its children <Route>s and
+            renders the first one that matches the current URL. */}
+        <Switch>
+          <Route path="/create">
+            <CreateAccountForm />
+          </Route>
+          <Route path="/forgotpassword">
+            <ForgotPasswordForm />
+          </Route>
+          <Route path="/">
+            <LoginForm />
+          </Route>
+        </Switch>
+      </div>
+    </Router>
       </header>
     </div>
   );
 }
+
 
 export default App;
